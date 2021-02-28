93a65e884218ae4079920631bbfc9d0df7dc7707
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2021-02-28 13:15:39 +0700

    fix config db

--- database/telegramapp.sql
+++ database/telegramapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2021 at 07:16 PM
+-- Generation Time: Feb 28, 2021 at 07:11 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
