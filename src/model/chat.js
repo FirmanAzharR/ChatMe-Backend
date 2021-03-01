b5544cb536024a9738186568f9fe03412d380817
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2021-03-01 09:07:46 +0700

    fix model all sql db

--- src/model/chat.js
+++ src/model/chat.js
@@ -3,7 +3,7 @@ module.exports = {
   getChatList: (id) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT room_chat.key_room, room_chat.user1,room_chat.user2, profile.user_fullname,profile.user_photo FROM PROFILE JOIN room_chat ON profile.user_id = room_chat.user2 WHERE room_chat.user1 = ${id} GROUP BY room_chat.key_room ORDER BY(room_chat.create_at) DESC`,
+        `SELECT room_chat.key_room, room_chat.user1,room_chat.user2, profile.user_fullname,profile.user_photo FROM profile JOIN room_chat ON profile.user_id = room_chat.user2 WHERE room_chat.user1 = ${id} GROUP BY room_chat.key_room ORDER BY(room_chat.create_at) DESC`,
         (error, result) => {
           if (!error) {
             resolve(result)
@@ -17,7 +17,7 @@ module.exports = {
   getLastMsg: (user_id1, key_room) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT chat.id_chat, chat.message,DATE_FORMAT(chat.create_at,"%d/%m/%Y")  AS dates,TIME_FORMAT(chat.create_at, "%H:%i") AS times FROM PROFILE JOIN room_chat ON profile.user_id = room_chat.user2 JOIN chat ON room_chat.key_room = chat.key_room WHERE room_chat.user1 = ${user_id1} AND room_chat.key_room = ${key_room} ORDER BY (chat.id_chat) DESC LIMIT 1`,
+        `SELECT chat.id_chat, chat.message,DATE_FORMAT(chat.create_at,"%d/%m/%Y")  AS dates,TIME_FORMAT(chat.create_at, "%H:%i") AS times FROM profile JOIN room_chat ON profile.user_id = room_chat.user2 JOIN chat ON room_chat.key_room = chat.key_room WHERE room_chat.user1 = ${user_id1} AND room_chat.key_room = ${key_room} ORDER BY (chat.id_chat) DESC LIMIT 1`,
         (error, result) => {
           if (!error) {
             resolve(result)
@@ -31,7 +31,7 @@ module.exports = {
   getChat: (data) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT profile.user_fullname,profile.user_photo,room_chat.key_room, room_chat.user1,room_chat.user2,chat.id_sender,chat.id_reciver,chat.message,DATE_FORMAT(chat.create_at,"%d/%m/%Y")  AS dates,TIME_FORMAT(chat.create_at, "%H:%i") AS times FROM PROFILE JOIN room_chat ON profile.user_id = room_chat.user2 JOIN chat ON room_chat.key_room = chat.key_room WHERE room_chat.user1 = ${data.id} AND room_chat.key_room = ${data.key}`,
+        `SELECT profile.user_fullname,profile.user_photo,room_chat.key_room, room_chat.user1,room_chat.user2,chat.id_sender,chat.id_reciver,chat.message,DATE_FORMAT(chat.create_at,"%d/%m/%Y")  AS dates,TIME_FORMAT(chat.create_at, "%H:%i") AS times FROM profile JOIN room_chat ON profile.user_id = room_chat.user2 JOIN chat ON room_chat.key_room = chat.key_room WHERE room_chat.user1 = ${data.id} AND room_chat.key_room = ${data.key}`,
         (error, result) => {
           if (!error) {
             resolve(result)
@@ -45,7 +45,7 @@ module.exports = {
   getRoom: (key_room, user2) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT room_chat.user2,room_chat.key_room, profile.user_photo, profile.user_status, profile.user_fullname FROM room_chat JOIN PROFILE ON room_chat.user2 = profile.user_id WHERE room_chat.key_room = ${key_room} AND room_chat.user2 = ${user2}`,
+        `SELECT room_chat.user2,room_chat.key_room, profile.user_photo, profile.user_status, profile.user_fullname FROM room_chat JOIN profile ON room_chat.user2 = profile.user_id WHERE room_chat.key_room = ${key_room} AND room_chat.user2 = ${user2}`,
         (error, result) => {
           if (!error) {
             resolve(result)
